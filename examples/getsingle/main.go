6fc590ac58a353594b4fa3469cf9e70390b61c95
manuelbcd <manuelbcd@gmail.com>
2021-02-23 18:56:15 +0000

    Added getsingle example (Get a single work-package)

---
+++ examples/getsingle/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	openproj "go-openproject"
+	"io/ioutil"
+)
+
+const openProjURL = "https://community.openproject.org/"
+
+func main() {
+
+	client, err := openproj.NewClient(nil, openProjURL)
+	if err != nil {
+		fmt.Printf("\nerror: %v\n", err)
+		return
+	}
+
+	wpResponse, resp, err := client.WorkPackage.Get("36353", nil)
+	if err != nil {
+		body, err := ioutil.ReadAll(resp.Body)
+		fmt.Printf(string(body))
+		panic(err)
+	}
+
+	// Output specific fields from response
+	fmt.Printf("\n\nSubject: %s \nDescription: %s\n\n", wpResponse.Subject, wpResponse.Description.Raw)
+
+	// Raw output of the whole object (only for debug)
+	fmt.Printf(prettyPrint(wpResponse))
+}
+
+func prettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
+
