9bbe198befb1ae9791ee535939236494ece446e2
manuelbcd <manuelbcd@gmail.com>
2021-03-16 22:49:33 +0000

    Download attachment test

--- attachment_test.go
+++ attachment_test.go
@@ -34,3 +34,31 @@ func TestAttachmentService_Get(t *testing.T) {
 		t.Errorf("Unexpected attachment filename %s", attachment.FileName)
 	}
 }
+
+func TestAttachmentService_Download(t *testing.T) {
+	setup()
+	defer teardown()
+	testAPIEdpoint := "/api/v3/attachments/5/content"
+
+	raw, err := ioutil.ReadFile("./mocks/get/download-attachment-file.jpg")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	testMux.HandleFunc(testAPIEdpoint, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		testRequestURL(t, r, testAPIEdpoint)
+		fmt.Fprint(w, string(raw))
+	})
+
+	file, err := testClient.Attachment.Download("5")
+	if err != nil {
+		t.Errorf("Error given: %s", err)
+	}
+	if file == nil {
+		t.Error("Expected file (download). File is nil")
+		return
+	}
+	if len(*file) != 111186 {
+		t.Errorf("Unexpected downloaded filesize %d", len(*file))
+	}
+}
