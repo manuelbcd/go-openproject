56810a2c70ca875dbccc5120cad80f22af5a352e
manuelbcd <manuelbcd@gmail.com>
2021-03-29 20:18:34 +0000

    Improved openproject.go tests

--- openproject_test.go
+++ openproject_test.go
@@ -1,10 +1,18 @@
 package openproject
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"reflect"
 	"strings"
 	"testing"
+	"time"
+)
+
+const (
+	testOpenProjectInstanceURL = "https://community.openproject.org/"
 )
 
 var (
@@ -73,5 +81,112 @@ func TestNewClient_WrongUrl(t *testing.T) {
 	}
 	if c != nil {
 		t.Errorf("Expected no client. Got %+v", c)
+	}
+}
+
+func TestNewClient_WithHttpClient(t *testing.T) {
+	httpClient := http.DefaultClient
+	httpClient.Timeout = 10 * time.Minute
+
+	c, err := NewClient(httpClient, testOpenProjectInstanceURL)
+	if err != nil {
+		t.Errorf("Got an error: %s", err)
+	}
+	if c == nil {
+		t.Error("Expected a client. Got none")
+		return
+	}
+	if !reflect.DeepEqual(c.client, httpClient) {
+		t.Errorf("HTTP clients are not equal. Injected %+v, got %+v", httpClient, c.client)
+	}
+}
+
+func TestNewClient_WithServices(t *testing.T) {
+	c, err := NewClient(nil, testOpenProjectInstanceURL)
+
+	if err != nil {
+		t.Errorf("Got an error: %s", err)
+	}
+	if c.Authentication == nil {
+		t.Error("No AuthenticationService provided")
+	}
+	if c.WorkPackage == nil {
+		t.Error("No WorkpackageService provided")
+	}
+	if c.Project == nil {
+		t.Error("No ProjectService provided")
+	}
+	if c.User == nil {
+		t.Error("No UserService provided")
+	}
+}
+
+func TestCheckResponse(t *testing.T) {
+	codes := []int{
+		http.StatusOK, http.StatusPartialContent, 299,
+	}
+
+	for _, c := range codes {
+		r := &http.Response{
+			StatusCode: c,
+		}
+		if err := CheckResponse(r); err != nil {
+			t.Errorf("CheckResponse throws an error: %s", err)
+		}
+	}
+}
+
+func TestClient_NewRequest(t *testing.T) {
+	c, err := NewClient(nil, testOpenProjectInstanceURL)
+	if err != nil {
+		t.Errorf("An error occurred. Expected nil. Got %+v.", err)
+	}
+
+	inURL, outURL := "api/v3/workpackages/", testOpenProjectInstanceURL+"api/v3/workpackages/"
+	inBody, outBody := &WorkPackage{ID: 1}, `{"id":1}`+"\n"
+	req, _ := c.NewRequest("GET", inURL, inBody)
+
+	// Test that relative URL was expanded
+	if got, want := req.URL.String(), outURL; got != want {
+		t.Errorf("NewRequest(%q) URL is %v, want %v", inURL, got, want)
+	}
+
+	// Test that body was JSON encoded
+	body, _ := ioutil.ReadAll(req.Body)
+	if got, want := string(body), outBody; got != want {
+		t.Errorf("NewRequest(%v) Body is %v, want %v", inBody, got, want)
+	}
+}
+
+func TestClient_NewRawRequest(t *testing.T) {
+	c, err := NewClient(nil, testOpenProjectInstanceURL)
+	if err != nil {
+		t.Errorf("An error occurred. Expected nil. Got %+v.", err)
+	}
+
+	inURL, outURL := "api/v3/workpackages/", testOpenProjectInstanceURL+"api/v3/workpackages/"
+
+	outBody := `{"id":1}` + "\n"
+	inBody := outBody
+	req, _ := c.NewRawRequest("GET", inURL, strings.NewReader(outBody))
+
+	// Test that relative URL was expanded
+	if got, want := req.URL.String(), outURL; got != want {
+		t.Errorf("NewRawRequest(%q) URL is %v, want %v", inURL, got, want)
+	}
+
+	// Test that body was JSON encoded
+	body, _ := ioutil.ReadAll(req.Body)
+	if got, want := string(body), outBody; got != want {
+		t.Errorf("NewRawRequest(%v) Body is %v, want %v", inBody, got, want)
+	}
+}
+
+func testURLParseError(t *testing.T, err error) {
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+	if err, ok := err.(*url.Error); !ok || err.Op != "parse" {
+		t.Errorf("Expected URL parse error, got %+v", err)
 	}
 }
